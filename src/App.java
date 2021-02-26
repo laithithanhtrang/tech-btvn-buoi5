cf5b8d28b021489a6db7ae5df6deb6e25c28fabc
TrangLTT <trangltt@edmicro.vn>
2021-02-26 15:33:01 +0700

    second commit

--- src/App.java
+++ src/App.java
@@ -1,13 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Student sv1 = new Student(21111, "Lại Trang", 7, 8);
+        Student sv1 = new Student();
+        sv1.code = 213294;
+        sv1.name ="Lại Trang";
+        sv1.theoryScore = 5;
+        sv1.praticeScore = 8;
         sv1.averageScore();
 
-        Student sv2 = new Student(21115, "Tororo", 7, 9);
+        Student sv2 = new Student();
+        sv2.code = 65583;
+        sv2.name ="Mèo lớn";
+        sv2.theoryScore = 9;
+        sv2.praticeScore = 8;
         sv2.averageScore();
 
-        Student sv3 = new Student(49843, "Tororo", 5, 9);
+        Student sv3 = new Student();
+        sv3.code = 213294;
+        sv3.name ="Mèo nhỏ";
+        sv3.theoryScore = 7;
+        sv3.praticeScore = 8;
         sv3.averageScore();
+
+
+        Student sv4 = new Student(49843, "Tororo", 5, 9);
+        sv4.averageScore();
         
     }
 }
